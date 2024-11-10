31f4809f604adeec3a046aabf53e20f6ae3baa6b
hiagoCf19 <hiagoferreira@id.uff.br>
2024-11-10 14:25:35 -0300

    feat: redirect user to Stripe checkout on plan selection

---
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Button } from "@/app/_components/ui/button";
+import { createStripeCheckout } from "../_actions/create-stripe-checkout";
+import { loadStripe } from "@stripe/stripe-js";
+const AcquirePlanButton = () => {
+  const handleAcquirePlanClick = async () => {
+    const { sessionId } = await createStripeCheckout();
+
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Missing STRIPE_PUBLISHABLE_KEY");
+    }
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+    if (!stripe) {
+      throw new Error("Failed to load Stripe");
+    }
+    await stripe.redirectToCheckout({ sessionId });
+  };
+  return (
+    <Button
+      className="w-full rounded-md font-bold"
+      onClick={handleAcquirePlanClick}
+    >
+      Adquirir plano
+    </Button>
+  );
+};
+
+export default AcquirePlanButton;
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,11 +1,66 @@
+"use server";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import NavBAr from "../_components/navbar";
+import { Card, CardContent, CardHeader } from "../_components/ui/card";
+import { CheckIcon, XIcon } from "lucide-react";
+import AcquirePlanButton from "./_components/acquire-plan-button";
 
 const Subscription = async () => {
   const { userId } = await auth();
   if (!userId) redirect("/login");
-  return <NavBAr />;
+
+  return (
+    <>
+      <NavBAr />
+      <div className="space-y-6 p-6">
+        <h1 className="text-2xl font-bold">Assinatura</h1>
+        <div className="flex w-2/4 gap-6">
+          <Card className="flex-1">
+            <CardHeader className="border-b border-solid py-8">
+              <h2 className="text-2xl font-semibold">Plano Básico</h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">0</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-6 py-8">
+              <div className="flex items-center gap-3">
+                <CheckIcon className="text-primary" />
+                <p>Apenas 10 transações por mês</p>
+              </div>
+              <div className="flex items-center gap-3">
+                <XIcon />
+                <p>Relatórios de IA</p>
+              </div>{" "}
+            </CardContent>
+          </Card>
+          <Card className="flex-1">
+            <CardHeader className="border-b border-solid py-8">
+              <h2 className="text-2xl font-semibold">Plano Premium</h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">19</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-6 py-8">
+              <div className="flex items-center gap-3">
+                <CheckIcon className="text-primary" />
+                <p>Transações ilimitadas</p>
+              </div>
+              <div className="flex items-center gap-3">
+                <CheckIcon className="text-primary" />
+                <p>Relatórios de IA</p>
+              </div>
+              <AcquirePlanButton />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Subscription;
